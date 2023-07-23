25c8f18a484d3017890e2dbea9a0ed89ff88815c
yaoqinchuan <1373992275@qq.com>
2023-07-23 21:03:26 +0800

    add mybatis plus

--- src/main/java/com/example/mybatisplus/config/MybatisPlusConfig.java
+++ src/main/java/com/example/mybatisplus/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package com.example.mybatisplus.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,10 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor getMybatisPlusInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        // 分页插件
         mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁插件
+        mybatisPlusInterceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
         return mybatisPlusInterceptor;
     }
 }
